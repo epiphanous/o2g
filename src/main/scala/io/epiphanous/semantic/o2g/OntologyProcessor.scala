8a0e775fecbcd3dfd8c55a2be28af7305a62443a
Robert Lyons <nextdude@gmail.com>
2019-03-15 08:57:07 -0400

    ensure connection types are distinct and sorted

--- src/main/scala/io/epiphanous/semantic/o2g/OntologyProcessor.scala
+++ src/main/scala/io/epiphanous/semantic/o2g/OntologyProcessor.scala
@@ -524,7 +524,11 @@ class OntologyProcessor(conf: Conf) extends LazyLogging {
          |}
          |""".stripMargin.trim)
 
-    val labels = sortByLocalName(connectionTypes).flatMap(t => unions.getOrElse(fieldTypes(t), List(t))).map(genIRI)
+    val labels =
+      sortByLocalName(
+        connectionTypes
+          .flatMap(t => unions.getOrElse(fieldTypes(t), List(t)))
+      ).map(genIRI)
     blankLine()
     emitLine(s"# A union of connection nodes")
     emitLine(s"union U_NodeObject = ${labels.mkString(" | ")}")
